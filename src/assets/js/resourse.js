678308ab9cc7309a07bc82fa1eaa99bf6c9d744d
ymsyncer@gmail.com <min@gmail>
2018-09-25 11:35:16 +0800

    上传

---
+++ src/assets/js/resourse.js
@@ -0,0 +1,137 @@
+import malebody from '../../assets/img/malebody/male.png';
+import femalebody from '../../assets/img/femalebody/female.png';
+
+import eye1 from '../../assets/img/eye/eye1.png';
+import eye2 from '../../assets/img/eye/eye2.png';
+import eye3 from '../../assets/img/eye/eye3.png';
+
+import object1 from '../../assets/img/object/object1.png';
+import object2 from '../../assets/img/object/object2.png';
+import object3 from '../../assets/img/object/object3.png';
+import object4 from '../../assets/img/object/object4.png';
+import object5 from '../../assets/img/object/object5.png';
+import object6 from '../../assets/img/object/object6.png';
+import object7 from '../../assets/img/object/object7.png';
+import object8 from '../../assets/img/object/object8.png';
+import object9 from '../../assets/img/object/object9.png';
+import object10 from '../../assets/img/object/object10.png';
+import object11 from '../../assets/img/object/object11.png';
+import object12 from '../../assets/img/object/object12.png';
+import object13 from '../../assets/img/object/object13.png';
+import object14 from '../../assets/img/object/object14.png';
+import object15 from '../../assets/img/object/object15.png';
+import object16 from '../../assets/img/object/object16.png';
+
+import malehair1 from '../../assets/img/malehair/malehair1.png';
+import malehair2 from '../../assets/img/malehair/malehair2.png';
+import malehair3 from '../../assets/img/malehair/malehair3.png';
+import malehair4 from '../../assets/img/malehair/malehair4.png';
+import malehair5 from '../../assets/img/malehair/malehair5.png';
+import malehair6 from '../../assets/img/malehair/malehair6.png';
+import malehair7 from '../../assets/img/malehair/malehair7.png';
+import malehair8 from '../../assets/img/malehair/malehair8.png';
+import malehair9 from '../../assets/img/malehair/malehair9.png';
+import malehair10 from '../../assets/img/malehair/malehair10.png';
+import malehair11 from '../../assets/img/malehair/malehair11.png';
+import malehair12 from '../../assets/img/malehair/malehair12.png';
+
+import maleshoes1 from '../../assets/img/maleshoes/maleshoes1.png';
+import maleshoes2 from '../../assets/img/maleshoes/maleshoes2.png';
+import maleshoes3 from '../../assets/img/maleshoes/maleshoes3.png';
+import maleshoes4 from '../../assets/img/maleshoes/maleshoes4.png';
+import maleshoes5 from '../../assets/img/maleshoes/maleshoes5.png';
+import maleshoes6 from '../../assets/img/maleshoes/maleshoes6.png';
+import maleshoes7 from '../../assets/img/maleshoes/maleshoes7.png';
+
+import malepants1 from '../../assets/img/malepants/malepants1.png';
+import malepants2 from '../../assets/img/malepants/malepants2.png';
+import malepants3 from '../../assets/img/malepants/malepants3.png';
+import malepants4 from '../../assets/img/malepants/malepants4.png';
+import malepants5 from '../../assets/img/malepants/malepants5.png';
+import malepants6 from '../../assets/img/malepants/malepants6.png';
+import malepants7 from '../../assets/img/malepants/malepants7.png';
+
+import malejacket1 from '../../assets/img/malejacket/malejacket1.png';
+import malejacket2 from '../../assets/img/malejacket/malejacket2.png';
+import malejacket3 from '../../assets/img/malejacket/malejacket3.png';
+import malejacket4 from '../../assets/img/malejacket/malejacket4.png';
+import malejacket5 from '../../assets/img/malejacket/malejacket5.png';
+import malejacket6 from '../../assets/img/malejacket/malejacket6.png';
+import malejacket7 from '../../assets/img/malejacket/malejacket7.png';
+
+import femalehair1 from '../../assets/img/femalehair/femalehair1.png';
+import femalehair2 from '../../assets/img/femalehair/femalehair2.png';
+import femalehair3 from '../../assets/img/femalehair/femalehair3.png';
+import femalehair4 from '../../assets/img/femalehair/femalehair4.png';
+import femalehair5 from '../../assets/img/femalehair/femalehair5.png';
+import femalehair6 from '../../assets/img/femalehair/femalehair6.png';
+import femalehair7 from '../../assets/img/femalehair/femalehair7.png';
+import femalehair8 from '../../assets/img/femalehair/femalehair8.png';
+import femalehair9 from '../../assets/img/femalehair/femalehair9.png';
+import femalehair10 from '../../assets/img/femalehair/femalehair10.png';
+import femalehair11 from '../../assets/img/femalehair/femalehair11.png';
+import femalehair12 from '../../assets/img/femalehair/femalehair12.png';
+
+import femaleshoes1 from '../../assets/img/femaleshoes/femaleshoes1.png';
+import femaleshoes2 from '../../assets/img/femaleshoes/femaleshoes2.png';
+import femaleshoes3 from '../../assets/img/femaleshoes/femaleshoes3.png';
+import femaleshoes4 from '../../assets/img/femaleshoes/femaleshoes4.png';
+import femaleshoes5 from '../../assets/img/femaleshoes/femaleshoes5.png';
+import femaleshoes6 from '../../assets/img/femaleshoes/femaleshoes6.png';
+
+import femalepants1 from '../../assets/img/femalepants/femalepants1.png';
+import femalepants2 from '../../assets/img/femalepants/femalepants2.png';
+import femalepants3 from '../../assets/img/femalepants/femalepants3.png';
+import femalepants4 from '../../assets/img/femalepants/femalepants4.png';
+import femalepants5 from '../../assets/img/femalepants/femalepants5.png';
+import femalepants6 from '../../assets/img/femalepants/femalepants6.png';
+
+import femalejacket1 from '../../assets/img/femalejacket/femalejacket1.png';
+import femalejacket2 from '../../assets/img/femalejacket/femalejacket2.png';
+import femalejacket3 from '../../assets/img/femalejacket/femalejacket3.png';
+import femalejacket4 from '../../assets/img/femalejacket/femalejacket4.png';
+import femalejacket5 from '../../assets/img/femalejacket/femalejacket5.png';
+import femalejacket6 from '../../assets/img/femalejacket/femalejacket6.png';
+
+
+import haibao1 from '../../assets/img/haibao/haibao1.png';
+import haibao2 from '../../assets/img/haibao/haibao2.png';
+import haibao3 from '../../assets/img/haibao/haibao3.png';
+import haibao4 from '../../assets/img/haibao/haibao4.png';
+import haibao5 from '../../assets/img/haibao/haibao5.png';
+import haibao6 from '../../assets/img/haibao/haibao6.png';
+import haibao7 from '../../assets/img/haibao/haibao7.png';
+import haibao8 from '../../assets/img/haibao/haibao8.png';
+import haibaoBg from '../../assets/img/haibao.png';
+import bg1 from '../../assets/img/bg/bg1.png';
+import bg2 from '../../assets/img/bg/bg2.png';
+import bg3 from '../../assets/img/bg/bg3.png';
+import bg4 from '../../assets/img/bg/bg4.png';
+
+import camaraImg from '../../assets/img/touxiang.png';
+import music from '../../assets/music/yinyue.mp3';
+import playImg from '../../assets/img/yieyue.png';
+import pauseImg from '../../assets/img/zanting.png';
+
+const allPic={
+  malebody:malebody,
+  femalebody:femalebody,
+  eye:{eye1,eye2,eye3},
+  malehair:{ malehair1, malehair2, malehair3, malehair4, malehair5, malehair6, malehair7, malehair8, malehair9 ,malehair10,malehair11,malehair12},
+  maleshoes:{ maleshoes1,maleshoes2,maleshoes3,maleshoes4,maleshoes5,maleshoes6,maleshoes7 },
+  malepants:{malepants1,malepants2,malepants3,malepants4,malepants5,malepants6,malepants7},
+  malejacket:{malejacket1,malejacket2,malejacket3,malejacket4,malejacket5,malejacket6,malejacket7},
+  femalehair:{femalehair1,femalehair2,femalehair3,femalehair4,femalehair5,femalehair6,femalehair7,femalehair8,femalehair9,femalehair10,femalehair11,femalehair12},
+  femaleshoes:{femaleshoes1,femaleshoes2,femaleshoes3,femaleshoes4,femaleshoes5,femaleshoes6},
+  femalepants:{femalepants1,femalepants2,femalepants3,femalepants4,femalepants5,femalepants6},
+  femalejacket:{femalejacket1,femalejacket2,femalejacket3,femalejacket4,femalejacket5,femalejacket6},
+  haibaos:[haibao1,haibao2,haibao3,haibao4,haibao5,haibao6,haibao7,haibao8],
+  selectBgs:{bg1,bg2,bg3,bg4},
+  object:{ object1, object2, object3, object4, object5, object6, object7, object8, object9, object10, object11, object12, object13, object14, object15, object16 },
+  haibaoBg:haibaoBg,
+  camaraImg:camaraImg,
+  music:music,
+  playImg:playImg,
+  pauseImg:pauseImg
+}
+ export default allPic
